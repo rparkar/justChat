a1bb8c826e08e7497d9237a18bf3ae2ada81a943
Rehan Parkar <parkarcamp@gmail.com>
2018-05-15 21:20:09 -0500

    created AuthService to register and login user with firebase

---
+++ justChat/services/AuthService.swift
@@ -0,0 +1,45 @@
+//
+//  AuthService.swift
+//  justChat
+//
+//  Created by Rehan Parkar on 2018-05-15.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static var instance = AuthService()
+    
+    
+    func registerUser(withEmail email: String, andPasswrd password: String, userCreateComplete: @escaping (_ status: Bool, _ error: Error? ) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            
+            guard let user = user else {
+                userCreateComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreateComplete(true, nil)
+            
+        }
+    }
+    
+    func loginUser(withEmail email: String, withPassword password: String, userLogedInComplete: @escaping (_ stauts: Bool, _ error : Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+            guard let user = user else {
+                userLogedInComplete(false, error)
+                return
+            }
+            
+            userLogedInComplete(true, nil)
+        }
+    }
+}
