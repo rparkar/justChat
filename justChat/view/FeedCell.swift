f36fec4df31569d5d5b4b392ae6919089b62113e
Rehan Parkar <parkarcamp@gmail.com>
2018-05-16 23:41:36 -0500

    created feed cell UI

--- justChat/view/FeedCell.swift
+++ justChat/view/FeedCell.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class FeedCell: UITableViewCell {
 
-
-
+    //outlets
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var contentMessageLabel: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, contentMessage: String) {
+        self.profileImage.image = profileImage
+        self.emailLabel.text = email
+        self.contentMessageLabel.text = contentMessage
+        
+    }
+    
 }
